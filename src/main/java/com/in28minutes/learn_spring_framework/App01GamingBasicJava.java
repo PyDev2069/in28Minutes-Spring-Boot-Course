f9cd5d2f63f9909e8221ddae6007d4277273249a
ROUNAK CHAKRABORTI <rounakchakraborti499@gmail.com>
2026-07-22 23:16:19 +0530

    Introduction to the Spring framework. wrote the first configuration class and Bean methods. Created the context and retrieved the beans in them

--- src/main/java/com/in28minutes/learn_spring_framework/App01GamingBasicJava.java
+++ src/main/java/com/in28minutes/learn_spring_framework/App01GamingBasicJava.java
@@ -1,9 +1,8 @@
 package com.in28minutes.learn_spring_framework;
 
 import com.in28minutes.learn_spring_framework.game.GameRunner;
-import com.in28minutes.learn_spring_framework.game.MarioGame;
 import com.in28minutes.learn_spring_framework.game.PacManGame;
-public class AppGamingBasicJava {
+public class App01GamingBasicJava {
     public static void main(String[] args) {
         var game = new PacManGame();
         //var game = new SuperContra();
