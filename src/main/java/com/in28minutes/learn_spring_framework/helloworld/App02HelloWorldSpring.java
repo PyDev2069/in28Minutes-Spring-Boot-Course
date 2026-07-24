05cdc08992802bdfa2133643f2461a5e8eda7300
ROUNAK CHAKRABORTI <rounakchakraborti499@gmail.com>
2026-07-25 01:41:23 +0530

    organized the code and made the game run via spring context

---
+++ src/main/java/com/in28minutes/learn_spring_framework/helloworld/App02HelloWorldSpring.java
@@ -0,0 +1,36 @@
+package com.in28minutes.learn_spring_framework.helloworld;
+import java.util.Arrays;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App02HelloWorldSpring {
+    public static void main(String[] args) {
+
+        try(
+            var context =  new AnnotationConfigApplicationContext(HelloWorldConfiguration.class)
+        ){
+            //* 1 Launch a spring context */        
+
+            //* 2  Configure the things we want spring to manage - @configuration class */
+
+            //* 3 Retrieve a bean from hello world configuration */
+            System.out.println(context.getBean("name"));
+            System.out.println(context.getBean("age"));
+            System.out.println(context.getBean("person"));
+            System.out.println(context.getBean("address"));        
+
+            //calling the mixed up beans
+            System.out.println(context.getBean("person2MethodCall"));
+            System.out.println(context.getBean("person3Parameters"));
+
+            //alternative
+            System.out.println(context.getBean(Address.class));
+
+
+            //? get bean definition names
+            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+        }
+        
+
+    }   
+}
