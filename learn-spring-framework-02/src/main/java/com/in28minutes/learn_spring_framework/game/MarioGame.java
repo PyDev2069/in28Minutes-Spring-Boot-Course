ccdfceb550642c006d1a75f42a8dc710fd279c83
ROUNAK CHAKRABORTI <rounakchakraborti499@gmail.com>
2026-07-28 00:15:32 +0530

    simplified the code in the second project. added components and primary and qualifier tags. Also learnt about the component scan annotation

--- learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/game/MarioGame.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/game/MarioGame.java
@@ -1,8 +1,13 @@
 package com.in28minutes.learn_spring_framework.game;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
 /**
  * MarioGame
  */
+@Component
+@Primary
 public class MarioGame implements GamingConsole{
     public void up(){
         System.out.println("Jump");
--- learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/game/SuperContra.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/game/SuperContra.java
@@ -1,8 +1,13 @@
 package com.in28minutes.learn_spring_framework.game;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
 /**
  * SuperContra
  */
+@Component
+@Qualifier("SuperContraGameQualifier")
 public class SuperContra implements GamingConsole {
     public void up(){
         System.out.println("Go UP");
